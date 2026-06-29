4fc76e20ee9843a0b2ec7733b017794b85e0cbf6
Saroj Dahal <isarojdahal@gmail.com>
2026-06-29 12:29:21 +0545

    add: upto Day 3 dir

---
+++ 02- Scikit Learn/main2.py
@@ -0,0 +1,14 @@
+my_tuple = ("Mandip","Prasanna","Saugat")
+
+# my_tuple[0] = "Test"
+print(my_tuple)
+
+
+student = {
+    "name":"Rashida",
+    "faculty":"BCA",
+    "semster":6,
+    "is_ncit_student":True,
+}
+
+print(student)
